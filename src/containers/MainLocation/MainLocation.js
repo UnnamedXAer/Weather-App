39d37b87f5f876b1ec548b96b4e3261d3aa92281
Kamil <unnamedxaer@gmail.com>
2020-01-07 21:23:45 +0100

    add darkskyjs package

--- src/containers/MainLocation/MainLocation.js
+++ src/containers/MainLocation/MainLocation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './MainLocation.css';
 import LocationSearch from '../../components/LocationSearch/LocationSearch';
+// import Darksky from 'darkskyjs';
 
 const mainLocalization = props => {
     return (
