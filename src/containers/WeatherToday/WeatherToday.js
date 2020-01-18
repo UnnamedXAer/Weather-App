059594f0db9e4b56d475463f0f935767dad7e2d8
Kamil <unnamedxaer@gmail.com>
2020-01-18 22:28:54 +0100

    Fix redirecting after city was selected
    Fixes redirecting to location search screen
    after location was selected

--- src/containers/WeatherToday/WeatherToday.js
+++ src/containers/WeatherToday/WeatherToday.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './WeatherToday.css';
+import { connect } from 'react-redux'
+import * as actions from '../../store/actions';
 
-const weatherToday = props => {
+const WeatherToday = ({ setRedirectToCurrentWeather }) => {
+
+    useEffect(() => {
+        setRedirectToCurrentWeather(false);
+    }, [setRedirectToCurrentWeather])
+
     return (
         <div className="weather-today">
             <h1>Weather Today</h1>
@@ -10,8 +17,19 @@ const weatherToday = props => {
     );
 };
 
-weatherToday.propTypes = {
-
+WeatherToday.propTypes = {
+    setRedirectToCurrentWeather: PropTypes.func
 };
 
-export default weatherToday;
+const mapStateToProps = (state) => ({
+    
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        setRedirectToCurrentWeather: (shouldRedirect) => dispatch(actions.setRedirectToCurrentWeather(shouldRedirect))
+    };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(WeatherToday);
