55b190141db5a4f41da66447175cadb16331b7ff
Kamil <unnamedxaer@gmail.com>
2020-01-18 22:27:05 +0100

    Update Footer with API Urls

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,15 @@ const Footer = () => {
 	return (
 		<footer className="footer">
 			<div className="footer__container">
-				<p>Icons made by <a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon" rel="noopener noreferrer" target="_blank"> www.flaticon.com</a></p>
-				<a className="footer-powered-by-dark-sky" href="_" rel="noopener noreferrer" target="_blank">
-					<img src={""} alt="Powered by ..." />
-				</a>
+				<p>Icons made by 
+					<a href="https://www.flaticon.com/authors/those-icons" title="Those Icons" target="_blank" rel="noopener noreferrer">
+						Those Icons
+					</a> from 
+					<a href="https://www.flaticon.com/" title="Flaticon" rel="noopener noreferrer" target="_blank"> www.flaticon.com</a>
+				</p>
+				<p>Locations are provided by 
+					<a href="http://geodb-cities-api.wirefreethought.com/" title="GeoDB Cities API" target="_blank" rel="noopener noreferrer">GeoDB Cities API</a>
+				</p>
 			</div>
 		</footer>
 	);
